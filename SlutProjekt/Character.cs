f028bd80db5a01c1da659cac46121be620e40089
Castor Dunkels <castordnk@gmail.com>
2023-03-08 13:30:47 +0100

    Start/pseudokod

---
+++ SlutProjekt/Character.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Character
+{
+    protected int health;
+
+    protected int attack;
+    
+    protected int speed;
+    
+    protected string name = "Billy";
+
+    
+}
---
+++ SlutProjekt/Program.cs
@@ -0,0 +1,8 @@
+﻿// Turn-based spel 
+// Alla gubbar har en "health" variabel och om den blir 0 så dör den gubben
+// Alla gubbar har en "attack", "damage" och "speed" variabel
+// Om "attack" variabeln av en gubbe som utför en attack är större än "speed" variabeln av gubben som blir attackerad så går den igenom 
+// Om "speed" variabeln var högre så kommer attacken att missa vilket gör att gubben 
+// Om attacken går igenom så förlorar den attackerade gubben lika mycket av sin "health" variabel som fiendens "damage" variabel
+// Alla dessa variablar förutom "health" kommer att bli slumpade mellan rundor 
+// Det kommer bli slumpade mellan olika störa tal beroende på val av klass 
